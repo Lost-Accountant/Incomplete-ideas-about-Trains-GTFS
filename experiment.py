9805758e774c117eeec30c67f1f5773303ef010c
Lost Accountant <50933696+Lost-Accountant@users.noreply.github.com>
2020-04-24 11:57:39 -0400

    Add files via upload

--- experiment.py
+++ experiment.py
@@ -1,4 +1,5 @@
 import pandas
+from datetime import datetime
 
 path = "E:\\Textbook\\4th Year\\CSC148\\tokyo trains\\"
 
@@ -6,4 +7,19 @@ weekday = 'weekday schedule.csv'
 
 df = pandas.read_csv(path + weekday, encoding = 'utf8')
 print(df)
-print(type(df['route_long_name'][1]))
+print(type(df['arrival_time'][1]))
+
+# str to datetime
+time = datetime.strptime(df['arrival_time'][1], '%H:%M:%S')
+print(time)
+print(type(time))
+
+print(df['arrival_time'][1] < df['arrival_time'][2])
+
+# day of the week
+# datetime to str
+print(datetime.datetime.now().strftime("%a"))
+
+# check date of the week
+# 0 is monday and 6 is sunday
+datetime.datetime.now().weekday()
--- timetable.py
+++ timetable.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class ScheduleEntry:
     """
     === ScheduleEntry ===
@@ -11,21 +13,36 @@ class ScheduleEntry:
     A ScheduleEntry must also be able to compare against one another based on the arrival time, letting trains arriving first be ranked first. It also needs to display useful information on the schedule board.
 
     === Attributes ===
-    :@type route_name: str
-    :@param route_name: a string that identifies the route of the arrival train
+    :@type route: str
+    :@param route: a string that identifies the route of the arrival train
     :@type direction: str
     :@param direction: a string that identifies the direction of the route mentioned above
     :@type arrival_time: str
+    :@param arrival_time: a string that identifies the arrival time of the upcoming train.
     """
-    def __init__(self, ):
+    def __init__(self, route = '', direction = '', arrival_time =''):
         """
+        Create a schedule entry
+        """
+        self.route = route,
+        self.direction = direction
+        self.arrival_time = datetime.strptime(arrival_time, '%H:%M:%S')
+        
 
-        """
+    def __str__(self):
+        pass
+    def __eq__(self, other):
+        pass
+    def __lt__(self, other):
+        pass
+    def __ne__(self, other):
+        pass
+    def __gt__(self, other):
         pass
 
 class ScheduleTable:
     """
-    First in First out system.
+    First in First out system. (Queue)
     """
     pass
 
